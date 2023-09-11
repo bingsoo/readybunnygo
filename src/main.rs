34deadc3fc003d4445c21e3745df45b7e559d850
bingsoo <bingsoo@bingsoo.com>
2023-09-10 19:55:12 -0700

    window setting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use bevy::{
+    prelude::*,
+    window::{CursorGrabMode, PresentMode, WindowLevel, WindowTheme},
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Ready Bunny Go!".into(),
+                resolution: (1920., 1080.).into(),
+                present_mode: PresentMode::AutoVsync,
+                // Tells wasm to resize the window according to the available canvas
+                fit_canvas_to_parent: true,
+                // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
+                prevent_default_event_handling: false,
+                window_theme: Some(WindowTheme::Dark),
+                ..default()
+            }),
+            ..default()
+        }),))
+        .run();
 }
