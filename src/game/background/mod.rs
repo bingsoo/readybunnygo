2d74f2517572546af540a5a11f022a31d8ced119
bingsoo <bingsoo@bingsoo.com>
2023-09-17 23:13:25 -0700

    speed control

--- src/game/background/mod.rs
+++ src/game/background/mod.rs
@@ -9,6 +9,6 @@ pub struct BackgroundPlugin;
 
 impl Plugin for BackgroundPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_background);
+        app.add_systems(Startup, spawn_background).add_systems(Update, update_background).add_systems(Update, update_tiles).add_systems(Update, speed_control);
     }
 }
--- src/game/background/systems.rs
+++ src/game/background/systems.rs
@@ -5,9 +5,51 @@ use rand::prelude::*;
 
 pub const BG_CELL_SIZE: f32 = 40.0;
 pub const LAND_ROW_COUNT: i32 = 20;
+pub const LAND_COL_COUNT: i32 = 60;
 
 #[derive(Component)]
-struct Parent;
+pub struct Parent;
+
+#[derive(Component)]
+pub struct Tile;
+
+#[derive(Resource)]
+pub struct GlobalData {
+    current_pos_y: f32,
+    speed: ScrollSpeed,
+}
+
+enum ScrollSpeed {
+    Speed1,
+    Speed2,
+    Speed3,
+    Speed4,
+    Speed5,
+}
+
+impl ScrollSpeed {
+    // Helper function to increment the speed
+    fn increment(&mut self) {
+        *self = match *self {
+            ScrollSpeed::Speed1 => ScrollSpeed::Speed2,
+            ScrollSpeed::Speed2 => ScrollSpeed::Speed3,
+            ScrollSpeed::Speed3 => ScrollSpeed::Speed4,
+            ScrollSpeed::Speed4 => ScrollSpeed::Speed5,
+            ScrollSpeed::Speed5 => ScrollSpeed::Speed5,
+        };
+    }
+
+    // Helper function to decrement the speed
+    fn decrement(&mut self) {
+        *self = match *self {
+            ScrollSpeed::Speed1 => ScrollSpeed::Speed1,
+            ScrollSpeed::Speed2 => ScrollSpeed::Speed1,
+            ScrollSpeed::Speed3 => ScrollSpeed::Speed2,
+            ScrollSpeed::Speed4 => ScrollSpeed::Speed3,
+            ScrollSpeed::Speed5 => ScrollSpeed::Speed4,
+        };
+    }
+}
 
 enum TileType {
     Normal1,
@@ -33,16 +75,28 @@ impl TileType {
     }
 }
 
+fn get_scroll_speed(speed: &ScrollSpeed) -> f32 {
+    match speed {
+        ScrollSpeed::Speed1 => 1.0,
+        ScrollSpeed::Speed2 => 2.5,
+        ScrollSpeed::Speed3 => 4.5,
+        ScrollSpeed::Speed4 => 7.5,
+        ScrollSpeed::Speed5 => 11.0,
+    }
+}
+
 fn add_bg_cell(commands: &mut Commands, asset_server: &Res<AssetServer>, loc: Vec3, parent: Entity, type_type: TileType) {
     let file_name = type_type.as_str();
-
     let sprite = commands
-        .spawn(SpriteBundle {
-            sprite: Sprite { custom_size: Some(Vec2::new(BG_CELL_SIZE, BG_CELL_SIZE)), ..default() },
-            texture: asset_server.load(file_name),
-            transform: Transform::from_translation(loc),
-            ..Default::default()
-        })
+        .spawn((
+            SpriteBundle {
+                sprite: Sprite { custom_size: Some(Vec2::new(BG_CELL_SIZE, BG_CELL_SIZE)), ..default() },
+                texture: asset_server.load(file_name),
+                transform: Transform::from_translation(loc),
+                ..Default::default()
+            },
+            Tile,
+        ))
         .id();
 
     commands.entity(parent).push_children(&[sprite]);
@@ -50,7 +104,7 @@ fn add_bg_cell(commands: &mut Commands, asset_server: &Res<AssetServer>, loc: Ve
 
 fn get_tile_type(row_pos: i32) -> TileType {
     let mut rng = rand::thread_rng();
-    let rand_number: usize = rng.gen_range(0..=1000);
+    let rand_number: usize = rng.gen_range(0..=500);
     let mut tile_type = TileType::Normal1;
 
     if row_pos == LAND_ROW_COUNT {
@@ -60,7 +114,7 @@ fn get_tile_type(row_pos: i32) -> TileType {
     }
 
     match rand_number {
-        6..=500 => tile_type = TileType::Normal2,
+        6..=200 => tile_type = TileType::Normal2,
         1 => tile_type = TileType::House1,
         2 => tile_type = TileType::House2,
         3 => tile_type = TileType::Tree1,
@@ -72,11 +126,35 @@ fn get_tile_type(row_pos: i32) -> TileType {
     tile_type
 }
 
+pub fn update_tiles(mut tile_position: Query<&mut Transform, With<Tile>>, global_data: Res<GlobalData>) {
+    for mut transform in &mut tile_position {
+        if transform.translation.y + global_data.current_pos_y + 580.0 < 0.0 {
+            transform.translation.y += BG_CELL_SIZE as f32 * LAND_COL_COUNT as f32;
+        }
+    }
+}
+
+pub fn update_background(mut parent_position: Query<&mut Transform, With<Parent>>, mut global_data: ResMut<GlobalData>) {
+    let mut transform = parent_position.single_mut();
+    transform.translation.y -= get_scroll_speed(&global_data.speed);
+    global_data.current_pos_y = transform.translation.y;
+}
+
+pub fn speed_control(keycode: Res<Input<KeyCode>>, mut global_data: ResMut<GlobalData>) {
+    if keycode.just_pressed(KeyCode::Up) {
+        global_data.speed.increment();
+    }
+    if keycode.just_pressed(KeyCode::Down) {
+        global_data.speed.decrement();
+    }
+}
+
 pub fn spawn_background(mut commands: Commands, asset_server: Res<AssetServer>, window_query: Query<&Window, With<PrimaryWindow>>) {
     let window = window_query.get_single().unwrap();
     println!("window size = {} / {} = {}", window.width(), BG_CELL_SIZE, window.width() / BG_CELL_SIZE);
     let row_side_count = (window.width() / BG_CELL_SIZE) as i32 / 2 + 1;
-    let col_count = (window.height() / BG_CELL_SIZE) as i32 + 1;
+    let col_count = LAND_COL_COUNT; //(window.height() / BG_CELL_SIZE) as i32 + 1;
+    let begin_y = window.height() * -1.0 * 0.5;
 
     println!("spawn background");
 
@@ -84,7 +162,7 @@ pub fn spawn_background(mut commands: Commands, asset_server: Res<AssetServer>, 
         .spawn((
             SpatialBundle {
                 visibility: Visibility::Visible,
-                transform: Transform::from_translation(Vec3::new(0., window.height() * -1.0 * 0.5, 0.)),
+                transform: Transform::from_translation(Vec3::new(0., begin_y, 0.)),
                 ..default()
             },
             Parent,
@@ -101,4 +179,7 @@ pub fn spawn_background(mut commands: Commands, asset_server: Res<AssetServer>, 
             add_bg_cell(&mut commands, &asset_server, loc_left, parent, get_tile_type(j));
         }
     }
+
+    commands.insert_resource(GlobalData { current_pos_y: begin_y, speed: ScrollSpeed::Speed1 });
+    println!("starting pos y = {}", begin_y);
 }
