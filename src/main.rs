3bb5a1ec67adf65e5afdf4d0e332337a994cfbb5
bingsoo <bingsoo@bingsoo.com>
2023-10-02 21:13:36 -0700

    code clean

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@
 mod game;
 use game::GamePlugin;
 
+// modules
 mod components;
+
+// systems and etc
 mod prelude {
     pub use crate::components::*;
     pub use bevy::prelude::*;
@@ -13,32 +16,24 @@ mod prelude {
 use prelude::*;
 
 fn setup(mut commands: Commands) {
-    commands
-        .spawn(Camera2dBundle {
-            camera: Camera { ..default() },
-            ..default()
-        })
-        .insert(GameCamera);
+    let cam = Camera2dBundle::default();
+    commands.spawn((GameCamera, cam));
 }
 
 fn main() {
     App::new()
-        .add_plugins(
-            DefaultPlugins
-                .set(ImagePlugin::default_nearest())
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        title: "Ready Bunny Go!".into(),
-                        resolution: (1920., 1080.).into(),
-                        present_mode: PresentMode::AutoVsync,
-                        fit_canvas_to_parent: true,
-                        prevent_default_event_handling: false,
-                        window_theme: Some(WindowTheme::Dark),
-                        ..default()
-                    }),
-                    ..default()
-                }),
-        )
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()).set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Ready Bunny Go!".into(),
+                resolution: (1920., 1080.).into(),
+                present_mode: PresentMode::AutoVsync,
+                fit_canvas_to_parent: true,
+                prevent_default_event_handling: false,
+                window_theme: Some(WindowTheme::Dark),
+                ..default()
+            }),
+            ..default()
+        }))
         .add_systems(Startup, setup)
         .add_plugins(GamePlugin)
         .add_systems(Update, bevy::window::close_on_esc)
