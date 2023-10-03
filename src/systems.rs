451f351c42ae33793b925f00004325cfc836a732
bingsoo <bingsoo@bingsoo.com>
2023-10-02 20:44:28 -0700

    delete comment

--- src/systems.rs
+++ src/systems.rs
@@ -1,20 +1,8 @@
-//use bevy::app::AppExit;
 use bevy::prelude::*;
-//use bevy::window::PrimaryWindow;
 
 use crate::GameCamera;
 
-pub fn spawn_camera(
-    mut commands: Commands,
-    //window_query: Query<&Window, With<PrimaryWindow>>
-) {
-    //let window = window_query.get_single().unwrap();
-
-    // commands.spawn(Camera2dBundle {
-    //     transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
-    //     ..default()
-    // });
-
+pub fn spawn_camera(mut commands: Commands) {
     commands.spawn((
         Camera2dBundle {
             camera: Camera { ..default() },
